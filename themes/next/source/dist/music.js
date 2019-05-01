4468853ba556a65cad3aad45f3a89b359cddda3d
unlimitbladeworks <13121347080@sina.cn>
2019-05-01 17:56:44 +0800

    modify

---
+++ themes/next/source/dist/music.js
@@ -0,0 +1,70 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    mini: false,
+    autoplay: false,
+    theme: '#FADFA3',
+    loop: 'all',
+    order: 'random',
+    preload: 'auto',
+    volume: 0.7,
+    mutex: true,
+    listFolded: false,
+    listMaxHeight: 90,
+    lrcType: 3,
+    audio: [
+    {
+        name: "Coming Home",
+        artist: 'Bo Bruce&Dash Berlin',
+        url: 'http://www.ytmp3.cn/down/53283.mp3',
+        cover: 'http://p1.music.126.net/QGb9Vtyw7qHS00uEvPfM6g==/843325418547559.jpg?param=130y130',
+      },
+      {
+        name: "爱如空气",
+        artist: '孙俪',
+        url: 'http://www.ytmp3.cn/down/47325.mp3',
+        cover: 'http://p1.music.126.net/-_6mcI4VV5IKaiwhUAytbg==/1791104441647901.jpg?param=130y130',
+      },
+      {
+        name: "破碎的世界",
+        artist: 'まじ娘',
+        url: 'http://www.ytmp3.cn/down/60309.mp3',
+        cover: 'http://p1.music.126.net/8ZRSyI0ZN_4ah8uzsNd1mA==/2324367581169008.jpg?param=130y130',
+      },
+      {
+        name: '惊蛰',
+        artist: '音阙诗听/王梓钰',
+        url: 'http://www.ytmp3.cn/down/48755.mp3',
+        cover: 'http://p1.music.126.net/5MmXpaP9r88tNzExPGMI8Q==/109951163370350985.jpg?param=130y130',
+      },
+      {
+        name: '永遠のランナー (永远的跑垒员)',
+        artist: '芹澤廣明 (せりざわ ひろあき)',
+        url: 'http://www.ytmp3.cn/down/44310.mp3',
+        cover: 'http://p1.music.126.net/5MmXpaP9r88tNzExPGMI8Q==/109951163370350985.jpg?param=130y130',
+      },
+      {
+        name: 'Do You Hear The People Sing？',
+        artist: 'Aaron Tveit',
+        url: 'http://www.ytmp3.cn/down/58197.mp3',
+        cover: 'http://p1.music.126.net/5MmXpaP9r88tNzExPGMI8Q==/109951163370350985.jpg?param=130y130',
+      },
+      {
+        name: 'be careful',
+        artist: 'cardi b',
+        url: 'http://www.ytmp3.cn/down/46395.mp3',
+        cover: 'http://p1.music.126.net/5MmXpaP9r88tNzExPGMI8Q==/109951163370350985.jpg?param=130y130',
+      },
+      {
+        name: '小幸运',
+        artist: '田馥甄',
+        url: 'http://www.ytmp3.cn/down/55491.mp3',
+        cover: 'http://p1.music.126.net/5MmXpaP9r88tNzExPGMI8Q==/109951163370350985.jpg?param=130y130',
+      },
+      {
+        name: '哼',
+        artist: '冯提莫',
+        url: 'http://www.ytmp3.cn/down/46744.mp3',
+        cover: 'http://p1.music.126.net/5MmXpaP9r88tNzExPGMI8Q==/109951163370350985.jpg?param=130y130',
+      }
+    ]
+});
